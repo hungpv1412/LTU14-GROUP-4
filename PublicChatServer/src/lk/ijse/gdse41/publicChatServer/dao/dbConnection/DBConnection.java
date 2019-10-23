e22ddc562fdf9ad884a135650b446e2346071248
hungpv1412 <hungpv1412@gmail.com>
2019-10-23 23:26:57 +0700

    ChatServer

---
+++ PublicChatServer/src/lk/ijse/gdse41/publicChatServer/dao/dbConnection/DBConnection.java
@@ -0,0 +1,32 @@
+package lk.ijse.gdse41.publicChatServer.dao.dbConnection;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by oshan on 18-Nov-17.
+ */
+public class DBConnection {
+    public static Connection getConnection() throws ClassNotFoundException,SQLException{
+        Properties dbPro=new Properties();
+//        InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("settings.properties");
+        try {
+            InputStream input = new FileInputStream(new File("PublicChatServer/dbSettings/settings.properties"));
+            dbPro.load(input);
+            System.out.println("Pass : "+dbPro.getProperty("password"));
+            String setDB=String.format("jdbc:mysql://%s:%s/%s", dbPro.getProperty("ip"),dbPro.getProperty("port"),dbPro.getProperty("database"));
+            System.out.println(setDB);
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection= DriverManager.getConnection(setDB,dbPro.getProperty("username"),dbPro.getProperty("password"));
+            System.out.println("Connected?");
+            return connection;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
